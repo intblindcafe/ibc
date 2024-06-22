edefd75af9c0cc5edb6e9b219191b54335edeffa
intblindcafe <171253207+intblindcafe@users.noreply.github.com>
2024-06-22 16:54:24 -0400

    Add files via upload

--- assets/scripts/footer.js
+++ assets/scripts/footer.js
@@ -1,8 +1,11 @@
-// footer.js
 document.addEventListener("DOMContentLoaded", function() {
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
+
+            // Now that the footer is loaded, set the current year
+            const currentYear = new Date().getFullYear();
+            document.getElementById('currentYear').textContent = currentYear;
         });
 });
